2a4060dfb61ff5a69464f234265733c47a41b606
craaen10 <colin.raaen@gmail.com>
2023-08-26 19:33:34 -0400

    August 26, 2023: finalized MergeSort.java with notes

--- MergeSort.java
+++ MergeSort.java
@@ -28,25 +28,23 @@ public class MergeSort {
             System.out.println(arrayToSort[x]);
         }
 
-        // Declare array and call Method to Sort Values
-        int[] sortedArray = sortArray(arrayToSort);
+        // Declare array to store sorted list and call Merge Sort Method to Sort Values
+        int startValue = 0;
+        int endValue = arrayToSort.length - 1;
+        mergeSort(arrayToSort, startValue, endValue);
 
-        // iterate through sorted array and print values
-        System.out.println("Sorted array values: ");
-        for (int x = 0; x < sortedArray.length; x++) {
-            System.out.println(sortedArray[x]);
-        }
     }
 
     // Method to sort array using Merge sort
     public static void mergeSort(int[] list, int start, int end) {
+        int i = 0; // counter variable to know when to print sorted values
         // If start equals end then array is either only 1 integer or end recursive calls
         if (start == end) {
-            return;
+            return; // exit method
         // There are two elements in the array, swap positions of integers if needed
-        } else if (start == end-1) {
+        } else if (start == end - 1) {
             if (list[start] <= list[end]) { // if start is less than or equal to end than return function w/o swap
-                return;
+                return; // exit method
             } else { // swap integers
                 int temp = list[start]; // store start value in temporary variable
                 list[start] = list[end]; // swap
@@ -60,48 +58,68 @@ public class MergeSort {
         mergeSort(list, start, start + mid);
         // Recursive method call for right half of array
         mergeSort(list, start + mid + 1, end);
-        // Call merge method to merge array lists
+        // Call merge method to merge array lists,
+        // inputting array, left half start in array, right half start in array and end value of array
         merge(list, start, start + mid, end);
+        i++; // increment i counter
+
+        // if last iteration of method, print sorted values
+        if (i == start + 1){
+            // iterate through sorted array and print values
+            System.out.println("Sorted array values: ");
+            for (int x = 0; x < list.length; x++) {
+                System.out.println(list[x]);
+            }
+        }
     }
 
+    // method helper to merge sublists
     private static void merge(int[] list, int leftHalfStart, int rightHalfStart, int end) {
+        // calculate size of left and right arrays
         int leftHalfSize = rightHalfStart - leftHalfStart + 1;
         int rightHalfSize = end - rightHalfStart;
 
+        // declare arrays for left half and right half
         int[] leftHalf = new int[leftHalfSize];
         int[] rightHalf = new int[rightHalfSize];
 
         // Loop through and assign left half values to leftHalf array
-        for (int i=0; i<leftHalfSize; ++i)
+        for (int i = 0; i < leftHalfSize; ++i)
             leftHalf[i] = list[leftHalfStart + i];
         // Loop through and assign right half values to rightHalf array
-        for (int j=0; j<rightHalfSize; ++j)
-            rightHalf[j] = list[rightHalfStart + 1+ j];
+        for (int j = 0; j < rightHalfSize; ++j)
+            rightHalf[j] = list[rightHalfStart + 1 + j];
 
-        int i = 0;
-        int j = 0;
+        // reset i and j values
+        int i = 0; // i is counter position of unsorted left half value
+        int j = 0; // j is counter position of unsorted right half value
 
-        int k = leftHalfStart;
+        int k = leftHalfStart;// k is counter for position of sorted array
         // Do while loop to merge rightHalf with leftHalf
+        // iterate through sublists until end of both lists have been reached
         while (i < leftHalfSize && j < rightHalfSize) {
-            // if leftHalf value is less than or equal to rightHalf value, then store leftHalf value first
+            // if the i leftHalf value is less than or equal to i rightHalf value, then store leftHalf value first
             if (leftHalf[i] <= rightHalf[j]) {
-                list[k] = leftHalf[i];
-                i++; // increment i
+                list[k] = leftHalf[i]; // store in location k of array which is next sorted position
+                i++; // increment i, next unsorted position of left half array
             } else {
-                // else rightHalf value is less than or equal to leftHalf value, then store rightHalf value first
-                list[k] = rightHalf[j];
-                j++;
+                // else i rightHalf value is less than or equal to i leftHalf value, then store rightHalf value first
+                list[k] = rightHalf[j]; // store in location k of array which is next sorted position
+                j++; // increment j, next unsorted position of right half array
             }
-            k++;
+            k++; // increment array location of sorted portion of array
         }
 
+        // if overall array has an odd number of values, store last remaining value from left half array in sorted list
+        // if it exists
         while (i < leftHalfSize) {
             list[k] = leftHalf[i];
             i++;
             k++;
         }
 
+        // if overall array has an odd number of values, store last remaining value from left half array
+        // in sorted list if it exists
         while (j < rightHalfSize) {
             list[k] = rightHalf[j];
             j++;

bc583a2466ed4e911cf873a3dddde23e779b9288
craaen10 <colin.raaen@gmail.com>
2023-09-02 00:38:47 -0400

    September 1, 2023: Started defining abstract classes with HW05 files

--- HW05/Impostor.java
+++ HW05/Impostor.java
@@ -1,8 +1,14 @@
-// Imposter Class
-public class Imposter {
+// Imposter Interface
+public interface Imposter {
 
     // freeze player P abstract method
+    public abstract class freeze{
+
+    }
 
     // sabatoge player p abstract method
+    public abstract class sabotage{
+
+    }
 
 }
